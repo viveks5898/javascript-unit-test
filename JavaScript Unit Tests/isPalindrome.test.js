057e55b38550ec00e394489b21442a65ac71eb97
viveks5898 <singh.vivek5898@gmail.com>
2024-02-20 02:54:15 +0530

    add palindrome test

--- JavaScript Unit Tests/isPalindrome.test.js
+++ JavaScript Unit Tests/isPalindrome.test.js
@@ -1,6 +1,11 @@
 const isPalindrome = require('./isPalindrome')
 
-test('check isPalindrome', () => { 
+test('check isPalindrome returns true', () => { 
 expect(isPalindrome("assa")).toBe(true)
 
  })
+
+ test('check isPalindrome returns false', () => { 
+    expect(isPalindrome("dev")).toBe(false)
+    
+     })
--- JavaScript Unit Tests/sum.test.js
+++ JavaScript Unit Tests/sum.test.js
@@ -3,3 +3,4 @@ const sum = require('./sum')
 test('add two numbers', async () => { 
     expect(sum(1,2)).toBe(3)
  })
+ 
